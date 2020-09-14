cc971e28263cf353c3211f20fc9ca627e1e16cb5
Rolin Buckoke <rolin_B@hotmail.com>
2020-09-13 18:08:26 -0700

    Scoreboard changes
    Added scoreboard needs some cleanup

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    public static int score = 0;
+    public TextMeshPro score_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_text.text = "Score: ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score_text.text = "Score: " + score;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Goals:
+
+Add double jump
+Add score board that follows scene
+    points on kills and collections
+
+maybe goals:
+reset jumps on token pickups? 
